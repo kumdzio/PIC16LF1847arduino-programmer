b82b80b7263231d1594f6834bd9a690fbec0f4e3
kumdzio <kumdzio@gmail.com>
2024-02-05 04:56:19 +0100

    GUI done

--- pc-part/PIC16LF1847 programmer/src/MainWindow.java
+++ pc-part/PIC16LF1847 programmer/src/MainWindow.java
@@ -1,58 +1,113 @@
 import javax.swing.*;
-import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.text.DateFormatter;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.ArrayList;
+
+import com.fazecast.jSerialComm.SerialPort;
+
 
 public class MainWindow {
-    public MainWindow() {
-        HexParser parser = new HexParser();
-        JFrame frame = new JFrame();//creating instance of JFrame
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        LayoutManager layout = new FlowLayout();
-        frame.setLayout(layout);
+    boolean fileSelected = false;
+    final JFileChooser fc = new JFileChooser();
+    private final Dimension DEFAULT_BUTTON_SIZE = new Dimension(100, 40);
+    private final JTextArea logArea = new JTextArea();
 
+    public MainWindow() {
+        JFrame frame = new JFrame();
+        frame.setTitle("PIC 16LF1847 programmer");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(200, 200, 500, 500);
+        frame.setLayout(new FlowLayout());
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+        mainPanel.setVisible(true);
+        frame.add(mainPanel);
+
+        JPanel hexFilePanel = new JPanel();
+        hexFilePanel.setLayout(new FlowLayout());
         JButton openFileButton = new JButton("Load hex file");
-        openFileButton.setBounds(0, 0, 100, 40);
-        openFileButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                final JFileChooser fc = new JFileChooser();
-                fc.setApproveButtonText("Open");
-                fc.setApproveButtonToolTipText("Open and parse selected file");
-                FileNameExtensionFilter filter = new FileNameExtensionFilter("Hex files", "hex");
-                fc.setFileFilter(filter);
-                int returnVal = fc.showOpenDialog(frame);
-                if (returnVal == JFileChooser.APPROVE_OPTION) {
-                    try {
-                        parser.parseFile(fc.getSelectedFile());
-                    } catch (FileNotFoundException ex) {
-                        JOptionPane.showMessageDialog(frame, "Selected file has not been found or do not have access rights to it",
-                                "File not found", JOptionPane.ERROR_MESSAGE);
-                    } catch (HexLineNotCorrectException ex) {
-                        JOptionPane.showMessageDialog(frame, "Unable to parse line in hex file. Problematic line: \"" + ex.getLine() + "\""
-                                , "Could not parse hex file", JOptionPane.ERROR_MESSAGE);
-                    } catch (WrongCheckSumException ex) {
-                        JOptionPane.showMessageDialog(frame, "Wrong checksum in line: \"" + ex.getLine() + "\"\n"
-                                + "Calculated: " + ex.getCalculated() + " Stored in line: " + ex.getSaved()
-                                , "Could not parse hex file", JOptionPane.ERROR_MESSAGE);
-                    }
-                    parser.printDecodedLines();
-                } else {
-                    JOptionPane.showMessageDialog(frame, "Please select proper HEX file",
-                            "File not chosen", JOptionPane.ERROR_MESSAGE);
-                }
+        JLabel chosenFileLabel = new JLabel("File not chosen yet..");
+        openFileButton.setSize(DEFAULT_BUTTON_SIZE);
+        openFileButton.addActionListener(e -> {
+            fc.setApproveButtonText("Open");
+            fc.setApproveButtonToolTipText("Open selected file");
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Hex files", "hex");
+            fc.setFileFilter(filter);
+            int returnVal = fc.showOpenDialog(frame);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                fileSelected = true;
+                chosenFileLabel.setText("Selected: "+fc.getSelectedFile().getName());
+            } else {
+                JOptionPane.showMessageDialog(frame, "Please select proper HEX file",
+                        "File not chosen", JOptionPane.ERROR_MESSAGE);
             }
         });
+        hexFilePanel.add(openFileButton);
+        hexFilePanel.add(chosenFileLabel);
+        mainPanel.add(hexFilePanel);
 
-        frame.add(openFileButton);
+        SerialPort[] comPorts = SerialPort.getCommPorts();
+        ArrayList<String> choices = new ArrayList<>();
+        for (SerialPort port : comPorts) {
+            choices.add(port.getDescriptivePortName());
+        }
 
 
-        frame.setBounds(200, 200, 400, 500);
+        JPanel comPortPanel = new JPanel();
+        comPortPanel.setLayout(new FlowLayout());
+        final JComboBox<Object> comPortsComboBox = new JComboBox<>(choices.toArray());
+        comPortsComboBox.setVisible(true);
+        comPortPanel.add(comPortsComboBox);
+        JButton testArduinoButton = new JButton("Test arduino");
+        testArduinoButton.addActionListener(e -> {
+            log("Start testing arduino...");
+        });
+        comPortPanel.add(testArduinoButton);
+        JButton testPicButton = new JButton("Test PIC");
+        testPicButton.addActionListener(e -> {
+            log("Start testing PIC...");
+        });
+        comPortPanel.add(testPicButton);
+        mainPanel.add(comPortPanel);
+
+
+        JPanel sendPanel = new JPanel();
+        sendPanel.setLayout(new FlowLayout());
+        JButton sendButton = new JButton("Start programming");
+        sendButton.setSize(DEFAULT_BUTTON_SIZE);
+        sendPanel.add(sendButton);
+        mainPanel.add(sendPanel);
+
+        setUpLogArea(mainPanel);
+
         frame.setVisible(true);
+    }
+
+    private void setUpLogArea(JPanel parent){
+        JPanel labelPanel = new JPanel();
+        labelPanel.setSize(parent.getSize().width,20);
+        BoxLayout labelPanelLayout = new BoxLayout(labelPanel,BoxLayout.LINE_AXIS);
+        labelPanelLayout.minimumLayoutSize(labelPanel);
+        labelPanel.setLayout(labelPanelLayout);
+        labelPanel.add(new JLabel("Application log: "));
+        parent.add(labelPanel);
+
+        JScrollPane logScrollPane = new JScrollPane(logArea);
+        logScrollPane.setWheelScrollingEnabled(true);
+        logScrollPane.setVerticalScrollBar(logScrollPane.createVerticalScrollBar());
+        logScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        logScrollPane.setPreferredSize(new Dimension(480,300));
+        parent.add(logScrollPane);
+    }
+
+    private void log(String text){
+        logArea.append(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss.SSS")));
+        logArea.append(": ");
+        logArea.append(text);
+        logArea.append("\n");
     }
 }  
--- pc-part/PIC16LF1847 programmer/src/NonHexCharException.java
+++
@@ -1,2 +0,0 @@
-public class NonHexCharException extends Exception{
-}
